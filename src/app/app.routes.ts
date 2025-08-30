e17b0fd149bc6e2b28fdfda1cfb0f56a9039a9be
narsinggurme <narsinggurme@gmail.com>
2025-08-30 08:16:35 +0530

    Updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,16 @@ import { CreateEmployeeComponent } from './create-employee/create-employee.compo
 import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
     { path: 'employees', component: EmployeeListComponent },
     { path: 'create-employee', component: CreateEmployeeComponent },
     { path: 'update-employee/:id', component: UpdateEmployeeComponent },
     { path: 'employee-details/:id', component: EmployeeDetailsComponent },
     { path: '', redirectTo: '/login', pathMatch: 'full' } // default redirect to login
+
 ];
---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  imports: [ReactiveFormsModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+  signupForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
+    this.signupForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      roles: ['NORMAL', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      this.http.post('http://localhost:8081/api/v1/signup', this.signupForm.value)
+        .subscribe({
+          next: () => this.router.navigate(['/login']),
+          error: err => console.error(err)
+        });
+    }
+  }
+
+}
