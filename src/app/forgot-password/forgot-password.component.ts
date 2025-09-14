e099ae4539204d083a3a9995879664ca795fdaae
narsinggurme <narsinggurme@gmail.com>
2025-09-14 09:04:01 +0530

    Updated code

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -15,6 +15,7 @@ export class ForgotPasswordComponent implements OnInit {
   forgotForm: FormGroup;
   step = 1;
   token: string = '';
+  loading = false;
 
   constructor(
     private fb: FormBuilder,
@@ -49,30 +50,52 @@ export class ForgotPasswordComponent implements OnInit {
 
   onSubmit() {
     if (this.step === 1) {
+      this.loading = true;
+
       const username = this.forgotForm.get('username')?.value;
       this.forgotService.sendResetLink(username).subscribe({
+        // next: res => {
+        //   this.loading = false;
+        //   console.log("api respnose:" + res);
+
+        //   alert(res.message || 'Reset link sent to your email!');
+        //   this.router.navigate(['/home']);
+        // },
         next: res => {
           console.log("api respnose:" + res);
-          alert(res.message || 'Reset link sent to your email!');
+          setTimeout(() => {
+            this.loading = false;
+            alert(res.message || 'Reset link sent to your email!');
+            this.router.navigate(['/home']);
+          }, 5000);
+
         },
-        error: err => alert(err.error?.message || 'Error sending reset link')
+        error: err => {
+          this.loading = false;
+          alert(err.error?.message || 'Error sending reset link')
+        }
       });
     } else {
       const newPass = this.forgotForm.get('newPassword')?.value;
       const confirmPass = this.forgotForm.get('confirmPassword')?.value;
 
       if (newPass !== confirmPass) {
+        this.loading = false;
         alert('Passwords do not match!');
         return;
       }
 
       this.forgotService.resetPasswordWithToken(this.token, newPass).subscribe({
         next: res => {
+          this.loading = false;
           console.log("api response: " + res)
           alert(res.message || 'Password reset successfully!');
           this.router.navigate(['/login']);
         },
-        error: err => alert(err.error?.message || 'Something went wrong')
+        error: err => {
+          this.loading = false; // stop loader
+          alert(err.error?.message || 'Something went wrong')
+        }
       });
     }
   }
